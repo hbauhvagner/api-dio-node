7f38f0fe91149a67eb26c665390c44422fdfd0ac
hbauhvagner <hbauhvagner@gmail.com>
2022-06-29 00:29:39 -0300

    Construindo as primeiras rotas

--- index.js
+++
@@ -1,5 +0,0 @@
-function soma(numero) {
-    return numero + 1;
-}
-
-console.log(soma('Vagner'));
--- index.ts
+++ index.ts
@@ -1,5 +1,11 @@
-function soma(numero: number) {
-    return numero + 1;
-}
+import express, { Request, Response } from 'express';
 
-console.log(soma(5));
+const server = express();
+
+server.get('/', (request: Request, response: Response) => {
+    return response.json({ mensagem: 'Bem vindo a nossa DIO API' });
+});
+
+server.listen(5000, () => {
+    console.log('Servidor on na porta 5000 http://localhost:5000/');
+});
