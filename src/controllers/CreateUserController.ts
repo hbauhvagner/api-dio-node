df3ec99b0c14f9a2d18ef4a72f88d3c2ea143df8
hbauhvagner <hbauhvagner@gmail.com>
2022-06-30 01:17:53 -0300

    Status code e enviando requisições

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -1,10 +1,22 @@
 import { Request, Response } from 'express';
+import { CreateUserService } from '../services/CreateUserService';
 
 class CreateUserController {
     handle(request: Request, response: Response) {
-        const { name } = request.body;
+        const createUserService = new CreateUserService();
 
-        return response.json({ message: `User ${name} created` });
+        const { name, email } = request.body;
+
+        if (name.length === 0 || email.length === 0) {
+            return response.status(400).json({ message: 'Preencha todos os campos' });
+        }
+
+        const user = createUserService.execute({
+            name,
+            email
+        })
+
+        return response.status(201).json({user});
     }
 }
 
